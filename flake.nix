d31a07cbfd06f1c5a424ddff082f799a11a3148f
Snifexx <matteobonesolii@gmail.com>
2026-02-27 18:12:19 +0100

    Fixup, modules and functions
    a

--- flake.nix
+++ flake.nix
@@ -18,8 +18,6 @@
         export SHELL="$(which fish)"
         $SHELL 
       '';
-      
-      libmoves = "c3c dynamic-lib ./datagen/out/moves.c3 --linux-crt /nix/store/1f0rwgif0bk36z8y2np96jyl7vl0926z-glibc-2.39-52/lib --linux-crtbegin /nix/store/dih8vf5naf93c0wcfxqa9pll3k7iv9bm-gcc-14-20241116/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1 --target elf-x64 -o libmoves --reloc=PIC";
     };
   };
 }
