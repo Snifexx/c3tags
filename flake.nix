a68a176bf70ad6b7dc3d9c333b0865042ec93de3
Snifexx <matteobonesolii@gmail.com>
2026-03-02 02:07:43 +0100

    Flake desc and build.c3
    a

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Shell";
+  description = "C3Tags dev shell and package";
 
   inputs = {
     nixpkgs.url = "nixpkgs/648f70160c03151bc2121d179291337ad6bc564b";
@@ -9,24 +9,18 @@
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-      name = "c3tags-test";
-      srcs = [ ./src ./libs ];
-      sourceRoot = ".";
+      pname = "c3tags";
+      version = "1.0";
+      src = ./.;
 
       buildInputs = with pkgs; [
         c3c c3-lsp
         tree-sitter
       ];
 
-      make3 = ./make3.c3;
-
-      postUnpack = ''
-        cp "$make3" .
-      '';
-
       buildPhase = ''
         ls
-        c3c compile -o c3tags src/* $make3 libs/tree-sitter/tree-sitter.c3i libs/tree-sitter-c3/tree_sitter_c3.c3i \
+        c3c compile -o c3tags src/* make3.c3 libs/tree-sitter/tree-sitter.c3i libs/tree-sitter-c3/tree_sitter_c3.c3i \
           -l tree-sitter -L ${pkgs.tree-sitter}/lib \
           -l tree-sitter-c3 -L libs/tree-sitter-c3 
       '';
